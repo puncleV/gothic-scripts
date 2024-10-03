67311679ec9bb5f5dd3d58e8be98272c850c29fc
Viktor <punkrus18@gmail.com>
2024-10-03 19:58:14 +0300

    Update more_karma_prayers.d
    upgrade karma script

--- more-karma-prayers/more_karma_prayers.d
+++ more-karma-prayers/more_karma_prayers.d
@@ -82,7 +82,8 @@ func void pc_prayshrine_pray_info() {
     };
 };
 
-func void pc_idolmobile_do_info() {
+
+func void rx_applybeliar_pray(var int ismobilestatue) {
     var int karma_per_prayer;
     karma_per_prayer = Menu_ReadInt("PUNCLEVUTILS", "ExtraBeliarKarmaPerPrayer");
 
@@ -90,77 +91,48 @@ func void pc_idolmobile_do_info() {
         karma_per_prayer = 1;
     };
 
-    if((hero.guild != gil_pal) && (hero.guild != gil_kdf)) {
-        if(is_prayday_beliar_now() == true) {
-            if((rx_necrosummonkarmamanatimes < 50) && (hero.guild == gil_kdm) && rx_isnecrosummoner()) {
-                rx_playeffect("SPELLFX_BELIARSHRINE", hero);
-                snd_play("DEM_RITUAL_02");
-                b_raiseattribute_bonus(hero, atr_mana_max, 1);
-                rx_necrosummonkarmamanatimes += 1;
-            };
-            if((hero.guild == gil_kdm) || rx_isdarkwarrior()) {
-                rx_restoremana(10000);
-                b_giveplayerxp(50);
-                rx_beliar(karma_per_prayer + 1);
-            }
-            else if(hero.guild == gil_ndm) {
-                b_giveplayerxp(25);
-            };
-            rx_beliar(karma_per_prayer);
-            rankpoints = rankpoints + 1;
-            prayidolday = wld_getday();
-            ai_print(print_beliarprayok);
+    if((hero.guild == gil_pal) || (hero.guild == gil_kdf)) {
+        ai_print(print_blessbeliarpraypalkdf);
+        return;
+    };
+    if(is_prayday_beliar_now() != true) {
+        ai_print(print_enoughtpray);
+        return;
+    };
+    if(ismobilestatue) {
+        b_say(hero, hero, "$HELLO_BELIAR");
+    };
+    rx_playeffect("SPELLFX_BELIARSHRINE", hero);
+    snd_play("DEM_RITUAL_01");
+    ai_print(print_beliarprayok);
+    prayidolday = wld_getday();
+    if((rx_necrosummonkarmamanatimes < 50) && (hero.guild == gil_kdm) && rx_isnecrosummoner()) {
+        if(rx_necrosummonkarmamanatimes == 49) {
+            b_raiseattribute_bonus(hero, atr_mana_max, 1);
+            rx_necrosummonkarmamanatimes += 1;
         }
         else {
-            ai_print(print_enoughtpray);
+            b_raiseattribute_bonus(hero, atr_mana_max, 2);
+            rx_necrosummonkarmamanatimes += 2;
+        };
+    };
+    if(rx_isheroindarkguild()) {
+        if(rx_isvampire()) {
+            rx_restoremanapercent(50);
+            rx_restorestaminapercent(50);
+            rx_restorehealthpercent(hero, 50);
+            rx_beliar(karma_per_prayer);
+        }
+        else {
+            rx_restoremanapercent(100);
+            rx_restorestaminapercent(50);
+            rx_restorehealthpercent(hero, 100);
+            b_giveplayerxp(50);
+            rx_beliar(karma_per_prayer * 2);
         };
     }
     else {
-        ai_print(print_blessbeliarpraypalkdf);
+        rx_beliar(karma_per_prayer);
     };
-};
-
-func void pc_prayidol_prayidol_info() {
-    var int karma_per_prayer;
-    karma_per_prayer = Menu_ReadInt("PUNCLEVUTILS", "ExtraBeliarKarmaPerPrayer");
-
-    if(karma_per_prayer == 0) {
-        karma_per_prayer = 1;
-    };
-
-
-    if((hero.guild != gil_pal) && (hero.guild != gil_kdf)) {
-        if(is_prayday_beliar_now() == true) {
-            rx_playeffect("SPELLFX_BELIARSHRINE", hero);
-            snd_play("DEM_RITUAL_01");
-            if((rx_necrosummonkarmamanatimes < 50) && (hero.guild == gil_kdm) && rx_isnecrosummoner()) {
-                if(rx_necrosummonkarmamanatimes == 49) {
-                    b_raiseattribute_bonus(hero, atr_mana_max, 1);
-                    rx_necrosummonkarmamanatimes += 1;
-                }
-                else {
-                    b_raiseattribute_bonus(hero, atr_mana_max, 2);
-                    rx_necrosummonkarmamanatimes += 2;
-                };
-            };
-            if((hero.guild == gil_kdm) || rx_isdarkwarrior()) {
-                rx_restoremanapercent(100);
-                b_giveplayerxp(50);
-                rx_beliar(karma_per_prayer + 1);
-            }
-            else if(hero.guild == gil_ndm) {
-                b_giveplayerxp(25);
-            };
-            rx_beliar(karma_per_prayer);
-            rankpoints = rankpoints + 1;
-            prayidolday = wld_getday();
-            ai_print(print_beliarprayok);
-        }
-        else {
-            ai_print(print_enoughtpray);
-        };
-    }
-    else {
-        ai_print(print_blessbeliarpraypalkdf);
-    };
+    rankpoints = rankpoints + 1;
 };
